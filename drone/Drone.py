09eca61f6055d0091d593107059ce4244be93a2c
Gilles Recouvreux <gilles.recouvreux@epita.fr>
2023-06-18 17:40:39 +0200

    fix(drone): reaname class Drone to class HarmonyWatcher and add attributes

--- drone/Drone.py
+++ drone/Drone.py
@@ -1,3 +1,7 @@
-class Drone:
-    def __init__(self):
+class HarmonyWatcher:
+    def __init__(self, id):
+        self.id = id
+        self.position = [0, 0, 0]
+        self.citizens = []
+        self.words = []
         print("Drone created")
