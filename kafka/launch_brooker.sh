339a6a2b927f1892e93d9094f2b42e63b45f2583
maxence.oden <maxence.oden@epita.fr>
2023-06-27 16:08:29 +0200

    feat(kafka): kafka on docker compose

--- kafka/launch_brooker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd kafka_2.13-3.4.0
-
-echo "Launch brooker"
-bin/kafka-server-start.sh config/server.properties
--- kafka/launch_zookeeper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd kafka_2.13-3.4.0
-
-echo -e "Launching zookeeper"
-bin/zookeeper-server-start.sh config/zookeeper.properties
